5c15792877c5121653c001393e459cd5fcc7357e
Rafael <34974103+RafaelJP13@users.noreply.github.com>
2025-05-17 22:05:30 -0300

    ✅: Add user model.

---
+++ src/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace Src\Models;
+
+class User {
+    public int $id;
+    public string $name;
+    public string $email;
+
+    public function __construct(int $id, string $name, string $email) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+    }
+}
